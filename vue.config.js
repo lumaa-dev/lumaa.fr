a37d272b9d90be9365d9406140bbe2b29d9f8866
Lumaa <nathan@ama49.fr>
2023-05-01 17:54:17 +0200

    ay

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV == "production" ? "/lumaa-dev.github.io/" : "/",
+  outputDir: "docs",
 
   pluginOptions: {
     i18n: {
