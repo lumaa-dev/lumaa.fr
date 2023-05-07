48256ab6ed8558993e3bb03b34f524d359ea6cd5
Lumaa <nathan@ama49.fr>
2023-05-07 16:08:17 +0200

    center + original website

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,11 +4,18 @@
     <p class="sub">{{ $t("motto") }}</p>
     <div class="sep"></div>
     <p>{{  $t("content.home.description") }}</p>
+    <a href="https://lumaa.brebond.com/" data-styled>Site actuel</a>
   </div>
 </template>
 
 <style scoped>
 h1 {
   font-size: 3.5vw;
+}
+
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
