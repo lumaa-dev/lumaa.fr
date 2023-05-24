b376b8bcf4a7a749f1ff31b11e24a73933303d80
Lumaa <nathan@ama49.fr>
2023-05-24 13:29:33 +0200

    Lumaa socials

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,17 @@
     <h1>Lumaa</h1>
     <p class="sub">{{ $t("motto") }}</p>
     <div class="sep"></div>
-    <p>{{  $t("content.home.description") }}</p>
-    <router-link v-bind:to="'/redirect?url=https://lumaa.brebond.com/'" data-styled>Site actuel</router-link>
+    <h2>{{ $t("content.home.socials") }}</h2>
+    <div class="links">
+      <a href="https://youtube.com/@lumaa_dev">YouTube</a>
+      <a href="https://twitch.com/lumaa_dev">Twitch</a>
+      <a href="https://instagram.com/lumaa_dev">Instagram</a>
+      <a href="https://tiktok.com/@lumaa_dev">TikTok</a>
+      <a href="https://github.com/lumaa-dev">GitHub</a>
+      <a href="https://modrinth.com/user/Lumaa">Modrinth</a>
+      <router-link to="/support">{{ $t("content.home.support") }}</router-link>
+      <router-link to="/community">{{ $t("content.home.community") }}</router-link>
+    </div>
   </div>
 </template>
 
