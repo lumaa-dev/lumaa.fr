8d1b4dda880b1181444ad0bbcd212485ba484d4d
Lumaa <nathan@ama49.fr>
2023-05-29 10:49:53 +0200

    npm i vue-head or smth

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import head from 'vue-head'
 import { i18n } from './i18n'
 import state from './store/index'
 
-createApp(App).use(i18n).use(state).use(router).mount('#app')
+createApp(App).use(i18n).use(state).use(router).use(head).mount('#app')
