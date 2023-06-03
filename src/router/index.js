64673fb7c7c036c89c97a92d327df54ea0972045
Lumaa <nathan@ama49.fr>
2023-06-03 12:00:29 +0200

    PROPER LINK YIPEEE

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import { nextTick } from "vue"
 import HomeView from '../views/HomeView.vue'
 import ModsView from '../views/ModsView.vue'
@@ -99,7 +99,7 @@ const customRedirections = [
 routes.push(...customRedirections)
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
