27a1cbc965c7dde1e0d8870754e3acf293d2529e
Lumaa <lumaa@lumaa.fr>
2024-03-05 16:11:30 +0100

    open graph and remove yt video and new links

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,39 +12,23 @@
 		<span style="gap: 1em" class="flex">
 			<p>Indie developer:<br />Swift, Vue, Java, and others</p>
 			<p>
-				Indie projects:<br /><a href="https://apps.lumaa.fr/app/threaded"
-					>Threaded</a
+				Projects:<br /><a href="https://apps.lumaa.fr/app/threaded">Threaded</a
 				>, <a href="https://apps.lumaa.fr/app/byheart">ByHeart</a>...
 			</p>
 		</span>
 		<div class="sep"></div>
-		<iframe
-			width="560"
-			height="315"
-			src="https://www.youtube.com/embed/uE666dV33Vc?si=43AJbRcLuVeAUYK4&amp;controls=0"
-			class="mob-hide"
-			title="YouTube video player"
-			frameborder="0"
-			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-			allowfullscreen
-		></iframe>
-		<div class="sep"></div>
 		<span style="gap: 1em" class="flex">
 			<p class="title">Follow me:</p>
+			<a href="https://read.cv/lumaa" class="br"><span class="label">read.cv</span></a>
 			<router-link to="/mastodon" class="br"
 				><span class="label">Mastodon</span></router-link
 			>
+			<a href="https://lemmy.world/u/lumaa" class="br"><span class="label">Lemmy</span></a>
 			<router-link to="/threads" class="br"
 				><span class="label">Threads</span></router-link
 			>
 			<router-link to="/github" class="br"
 				><span class="label">GitHub</span></router-link
-			>
-			<router-link to="/youtube" class="br"
-				><span class="label">YouTube</span></router-link
-			>
-			<router-link to="/twitch" class="br"
-				><span class="label">Twitch</span></router-link
 			>
 			<router-link to="/apps" class="br"
 				><span class="label">Apps</span></router-link
