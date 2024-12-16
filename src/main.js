b3045c3f213c1f7ac562480937790c8b8258cfde
lumaa-dev <lumaa@lumaa.fr>
2024-12-17 00:09:49 +0100

    removed supposedly build date

--- src/main.js
+++ src/main.js
@@ -2,11 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const buildDate = new Date()
-const formattedDate = `${String(buildDate.getDate()).padStart(2, '0')}/${String(buildDate.getMonth() + 1).padStart(2, '0')}/${buildDate.getFullYear()}`;
-
 const app = createApp(App)
 
-app.config.globalProperties.$buildDate = formattedDate
 app.use(router).mount('#app')
 
