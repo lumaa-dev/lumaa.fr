1a6d2cee2645ee429521dd0de0f251687e83409b
lumaa-dev <lumaa@lumaa.fr>
2025-08-11 15:42:42 +0200

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  
+  app: {
+    head: {
+      link: [
+        {
+          rel: "icon",
+          href: "/favicon.ico"
+        }
+      ]
+    }
+  }
 })
