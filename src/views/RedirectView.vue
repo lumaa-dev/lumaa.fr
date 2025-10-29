16ea41d53521fd803e706eb62c7300ef34e5ea69
lumaa-dev <lumaa@lumaa.fr>
2025-10-29 21:15:58 +0100

    format

--- src/views/RedirectView.vue
+++ src/views/RedirectView.vue
@@ -1,59 +1,60 @@
 <template>
-    <div>
-        <!-- <h1>{{ $t("content.redirect.redirecting") }}</h1>
+	<div>
+		<!-- <h1>{{ $t("content.redirect.redirecting") }}</h1>
         <button data-styled @click="this.goBack" v-if="this.lastRoute != ''">{{ $t("content.redirect.back") }}</button> -->
-    </div>
+	</div>
 </template>
 
 <style scoped>
 div {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: calc(100vh - 81.5px);
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	height: calc(100vh - 81.5px);
 }
 
 button {
-    margin: 5vh;
+	margin: 5vh;
 }
 </style>
 
 <script>
-    export default {
-        data() {
-            return {
-                timeout: null,
-                lastRoute: ""
-            }
-        },
-        methods: {
-            getUrl() {
-                return this.$route.query.url == undefined ? "" : this.$route.query.url;
-            },
-            safeUrl(url) {
-                return url.startsWith("https");
-            },
-            goBack() {
-                this.$router.push(this.lastRoute)
-                clearTimeout(this.$data.timeout);
-            }
-        },
-        beforeMount() {
-            let url = this.getUrl()
-            console.log(url)
-            if (url.length < 1 || url == null) this.goBack();
-            if (!this.safeUrl(url) || url.length < 1) url = "/?error=unknown_unsafe_link";
+export default {
+	data() {
+		return {
+			timeout: null,
+			lastRoute: "",
+		};
+	},
+	methods: {
+		getUrl() {
+			return this.$route.query.url == undefined ? "" : this.$route.query.url;
+		},
+		safeUrl(url) {
+			return url.startsWith("https");
+		},
+		goBack() {
+			this.$router.push(this.lastRoute);
+			clearTimeout(this.$data.timeout);
+		},
+	},
+	beforeMount() {
+		let url = this.getUrl();
+		console.log(url);
+		if (url.length < 1 || url == null) this.goBack();
+		if (!this.safeUrl(url) || url.length < 1)
+			url = "/?error=unknown_unsafe_link";
 
-            // this.$data.timeout = setTimeout(() => {
-            //     window.location.href = url
-            // }, 1.5*1000);
-            window.location.href = url
-        },
-        beforeRouteEnter(to, from, next) {
-            next(vm => {
-                vm.lastRoute = from.path;
-            })
-        }
-    }
+		// this.$data.timeout = setTimeout(() => {
+		//     window.location.href = url
+		// }, 1.5*1000);
+		window.location.href = url;
+	},
+	beforeRouteEnter(to, from, next) {
+		next((vm) => {
+			vm.lastRoute = from.path;
+		});
+	},
+};
 </script>
--- src/views/WrappedView.vue
+++ src/views/WrappedView.vue
@@ -5,53 +5,50 @@
 			src="../assets/hero/icon_white.svg"
 			alt="Lumaa icon"
 			class="brand"
-			draggable="false"
-		/>
+			draggable="false" />
 		<p class="title">Lumaa Wrapped 2025</p>
 		<div class="sep"></div>
 		<span style="gap: 1em" class="flex">
-			<p>
-                You might wanna check out later... You know... December 2025...
-            </p>
+			<p>You might wanna check out later... You know... December 2025...</p>
 		</span>
 	</span>
 </template>
 
 <style scoped>
-	span.flex {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-	}
+span.flex {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
 
-    span.flex.row {
-        flex-direction: row;
-    }
+span.flex.row {
+	flex-direction: row;
+}
 
-    .newb {
-        gap: 35px;
-    }
+.newb {
+	gap: 35px;
+}
 
-    .newb img {
-        height: 250px;
-    }
+.newb img {
+	height: 250px;
+}
 
-	span.m {
-		height: 85vh;
-	}
+span.m {
+	height: 85vh;
+}
 
-	.title {
-		font-size: 2em;
-		padding: 1em;
-	}
+.title {
+	font-size: 2em;
+	padding: 1em;
+}
 
-	.c {
-		font-size: 1.75em;
-		font-weight: 500;
-	}
+.c {
+	font-size: 1.75em;
+	font-weight: 500;
+}
 
-	.br:not(a.br, button.br) {
-		height: 80vh;
-	}
+.br:not(a.br, button.br) {
+	height: 80vh;
+}
 </style>
